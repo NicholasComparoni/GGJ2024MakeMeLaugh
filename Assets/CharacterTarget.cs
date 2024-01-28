92726f89ac366a63667583c1f59af0cb153fd30c
Mike <mic.bellotti02@gmail.com>
2024-01-28 12:59:58 +0100

    fixed movement problem

--- Assets/CharacterTarget.cs
+++ Assets/CharacterTarget.cs
@@ -1,13 +1,23 @@
 using System;
+using InputAndMovement;
 using UnityEngine;
+using UnityEngine.Windows.WebCam;
 
 [RequireComponent(typeof(CharacterBehaviour))]
 public class CharacterTarget : Target
 {
     public ExclamativePoint _xclPoint;
+    private PlayerMovement _player;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerMovement>();
+    }
 
     public void StartDialogue()
     {
+        _player.StopVelocity();
+        _player.enabled = false;
         CharacterBehaviour._currentCharacterInteraction = gameObject.GetComponent<CharacterBehaviour>();
         CharacterBehaviour._currentCharacterInteraction.Speak(this);
     }
@@ -15,5 +25,6 @@ public class CharacterTarget : Target
     public void CloseDialogue()
     {
         DialogueCanvas.Instance.gameObject.SetActive(false);
+        _player.enabled = true;
     }
 }
