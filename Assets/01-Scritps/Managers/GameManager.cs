fe0509d4509c92525b44a50d3f44fa13a39bdbc2
Mike <mic.bellotti02@gmail.com>
2024-01-31 18:45:59 +0100

    FINALLY fix character randomization algorithm

--- Assets/01-Scritps/Managers/GameManager.cs
+++ Assets/01-Scritps/Managers/GameManager.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Game Settings")] 
-    [SerializeField]
-    private GameObject _chestPrefab;
-    [SerializeField]
-    private GameObject _chestWithKeyPrefab;
+    [SerializeField] private GameObject _chestPrefab;
+    [SerializeField] private GameObject _chestWithKeyPrefab;
 
     [Header("Live Parameters")] public int currentLevel = 1;
     public AudioSource currentSoundtrack;
@@ -35,20 +35,58 @@ public class GameManager : MonoBehaviour
     public void Play()
     {
         SpawnChests();
+        SpawnCharacters();
     }
 
     private void SpawnChests()
     {
         List<GameObject> spawnerList = GameObject.FindGameObjectsWithTag("Chest").ToList();
         List<GameObject> chestList = new();
-        GameObject chestWithKey;
         foreach (GameObject obj in spawnerList)
         {
             GameObject chest = Instantiate(_chestPrefab, obj.transform, false);
             chestList.Add(chest);
         }
+
         int randomIndex = Random.Range(0, chestList.Count);
         Instantiate(_chestWithKeyPrefab, spawnerList[randomIndex].transform, false);
         Destroy(chestList[randomIndex]);
+    }
+
+    private void SpawnCharacters()
+    {
+        List<GameObject> spawnerList = GameObject.FindGameObjectsWithTag("Character").ToList();
+        List<GameObject> characterList = new();
+        List<int> loadedCharacters = new();
+        string[] pathList = Directory.GetFiles("Assets/03-Prefabs/CharProps/LVL1/", "*.prefab", SearchOption.TopDirectoryOnly);
+        foreach (string path in pathList)
+        {
+            characterList.Add((GameObject)AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)));
+        }
+
+        for (int i = 0; i < spawnerList.Count; i++)
+        {
+            int charIndex;
+            bool isFree;
+            do
+            {
+                isFree = true;
+                charIndex = Random.Range(0, characterList.Count);
+                for (int j = 0; j < loadedCharacters.Count; j++)
+                {
+                    if (charIndex == loadedCharacters[j])
+                    {
+                        isFree = false;
+                        break;
+                    }
+                }
+            } while (!isFree);
+            loadedCharacters.Add(charIndex);
+        }
+
+        foreach (int index in loadedCharacters)
+        {
+            Debug.Log(index);
+        }
     }
 }
