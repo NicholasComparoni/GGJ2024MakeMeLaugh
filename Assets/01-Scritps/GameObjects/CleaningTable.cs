896c40b330b04167aa40d3ef73ad2918b1ae710c
fiordarancio <tono.ilaria@gmail.com>
2024-01-31 23:12:48 +0100

    Updated cleaning table and exclamation point for level 2

--- Assets/01-Scritps/GameObjects/CleaningTable.cs
+++ Assets/01-Scritps/GameObjects/CleaningTable.cs
@@ -8,29 +8,31 @@ public class CleaningTable : Target
     [SerializeField] private Collider2D _mytriggerus;
     [SerializeField] public OpenDoorCheck _myController;
 
-    [SerializeField] private SpriteRenderer _mysprite;
+    [SerializeField] private SpriteRenderer _mysprite; // the first dirty
 
-    [SerializeField] private Sprite _myspriteDirty1;
-    [SerializeField] private Sprite _myspriteDirty2;
+    // [SerializeField] private Sprite _myspriteDirty1;
+    // [SerializeField] private Sprite _myspriteDirty2;
     [SerializeField] private Sprite _myspriteClean;
     private bool _hasBeenCleaned = false;
 
     public bool HasBeenCleaned { get { return _hasBeenCleaned; } private set { } }
 
-    private bool isA;
+    // private bool isA;
     [SerializeField] private int _cleanedTimes = 0;
 
     public void Cleaning()
     {
-        _mysprite.sprite = isA ? _myspriteDirty1 : _myspriteDirty2;
-        isA = !isA;
-        _cleanedTimes++;
+        _mysprite.sprite = _myspriteClean;
+        _hasBeenCleaned = true;
+        // _mysprite.sprite = isA ? _myspriteDirty1 : _myspriteDirty2;
+        // isA = !isA;
+        // _cleanedTimes++;
 
-        if (_cleanedTimes == 5)
-        {
-            _hasBeenCleaned = true;
-            _myController.OpenDoor();
-            _mysprite.sprite = _myspriteClean;
-        }
+        // if (_cleanedTimes == 5)
+        // {
+        //     _hasBeenCleaned = true;
+            // _myController.OpenDoor();
+        //     _mysprite.sprite = _myspriteClean;
+        // }
     }
 }
